b73146106bff494e53d8f69fd8cbbd248d34af3c
Jed <jed@e-accent.com>
2019-02-04 14:58:18 +1030

    Update to Middleman V4

--- Gemfile
+++ Gemfile
@@ -1,16 +1,8 @@
-# If you do not have OpenSSL installed, update
-# the following line to use "http://" instead
 source 'https://rubygems.org'
 
-gem "middleman", "~>3.3.10"
-
-# Autoprefixr
-gem 'middleman-autoprefixer'
-
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
-
-# Susy grid system
-# gem 'sass-rails', '~> 4.0.3'
-gem 'sass-rails', '~> 5.0.0.beta1'
-gem 'susy'
+gem 'middleman', '~> 4.2'
+gem 'middleman-sprockets', '~> 4.1.0'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+gem 'middleman-livereload'
--- config.rb
+++ config.rb
@@ -1,71 +1,19 @@
 activate :livereload
 activate :autoprefixer
-
+activate :sprockets
 set :relative_links, true
-
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Change where Middleman looks for the partials to the "partials" folder
 
 helpers do
 
+  # Change where Middleman looks for the partials to the "partials" folder
   def render_partial( partial_name )
     partial "partials/#{partial_name}"
   end
 
 end
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
@@ -81,7 +29,5 @@ configure :build do
 
   # Use relative URLs
   activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  
 end
